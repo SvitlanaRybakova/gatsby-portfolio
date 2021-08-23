8c605ec377897933876c93dca35186c1a4f079d5
Svitlana <devop.edu@gmail.com>
2021-08-23 15:31:39 +0200

    fix the single project page(dynamic data)

--- src/pages/projects/{StrapiProject.title}.js
+++ src/pages/projects/{StrapiProject.title}.js
@@ -1,5 +1,6 @@
-import React from 'react'
+import React from "react"
 import { graphql } from "gatsby"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 import Seo from "../../components/Seo"
 
 export const query = graphql`
@@ -8,25 +9,33 @@ export const query = graphql`
       description
       title
       image {
-        url
+        localFile {
+          childImageSharp {
+            gatsbyImageData(placeholder: BLURRED, layout: CONSTRAINED)
+          }
+        }
       }
     }
   }
 `
 
-const ProjectTemplate = ({pageContext: {title}, data}) => {
- 
+const ProjectTemplate = ({ pageContext: { title }, data }) => {
+  //
+  const img = data.strapiProject.image.localFile
+  console.log(img)
   return (
     <>
       <Seo
         title={data.strapiProject.title.toUpperCase()}
         description={data.strapiProject.description}
         image={data.strapiProject.image.url}
-
       />
       <main className="project-template-page">
         <h2>{title}</h2>
-        <p>{data.strapiProject.description}</p>
+        <div className="content-wrapper">
+          <GatsbyImage image={getImage(img)} className="single-project-img" alt={title} />
+          <p className="single-project-description">{data.strapiProject.description}</p>
+        </div>
       </main>
     </>
   )
